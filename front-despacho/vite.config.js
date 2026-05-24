d7598f779f934dcc37fce327b109cf285297bdca
CaritoCelis <ca.celisa@duocuc.cl>
2026-05-23 20:33:35 -0400

    fix: actualizar vite.config y Dockerfile multi-stage frontend

--- front-despacho/vite.config.js
+++ front-despacho/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'https://qic534o8o0.execute-api.us-east-1.amazonaws.com',
@@ -12,5 +13,9 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
+  },
+  preview: {
+    host: true,
+    port: 5173
   }
 })
